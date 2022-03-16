8fe0048fc8f9f2ed227e1ccf90057a741f89e8ea
Abhinav Mishra <101630308+abhinavpgdac@users.noreply.github.com>
2022-03-16 14:46:45 +0530

    Add files via upload

---
+++ Pract1.java
@@ -0,0 +1,18 @@
+package march16;
+// Wap to swap two numbers
+
+public class Pract1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("a:- " + a +"," + "b:- " + b);
+		int temp;
+		temp=b;
+		b=a;
+		a=temp;
+		System.out.println("a:- " + a +"," + "b:- " + b);
+
+	}
+
+}
